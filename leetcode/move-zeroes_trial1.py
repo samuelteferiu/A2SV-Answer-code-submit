08ff9f82c923267c9c649c28a90a38fcddb6118c
Samuel Teferi <samuelflatie@gmail.com>
2026-03-05 21:55:52 +0300

    Add solution for Move Zeroes (trial 1)

--- leetcode/move-zeroes_trial1.py
+++ leetcode/move-zeroes_trial1.py
@@ -2,6 +2,15 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         ans=[]
         zeros=[]
+        nots=[]
         for i in nums:
-            
+            if i==0:
+                zeros.append(i)
+            else:
+                nots.append(i)
+        ans=nots+zeros
+        for  i in range(len(nums)):
+            nums[i]=ans[i]
+
+
         
